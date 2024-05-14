708ac35c7c3da213f9886cde5127ea2a3364296d
blakehulett7 <blake.hulett7@gmail.com>
2024-05-14 15:14:09 -0500

    Added textnode_to_htmlnode function. Still have to finish it for the img type. More to follow, get excited!

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 print("Hello World!")
 
 from textnode import TextNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
 
 def main():
-    dummy1 = TextNode("bold test", "bold", "boot.dev")
-    dummy2 = TextNode("bold tests", "bold", "boot.dev")
-    print(dummy1)
-    print(dummy2)
-    print(dummy1 == dummy2)
-    
-    
+    textnode = TextNode("This is a text node", "italic")
+    print(textnode_to_htmlnode(textnode))
+
+
+def textnode_to_htmlnode(textnode):
+    valid_types = ["text", "bold", "italic", "code", "link", "image"]
+    if textnode.text_type not in valid_types:
+        raise Exception("Invalid Type")
+    valid_tags = [None, "b", "i", "code", "a", "img"]
+    valid_props = [None, None, None, None, {"href": textnode.url}, {}]
+    type_index = valid_types.index(textnode.text_type)
+    return LeafNode(valid_tags[type_index], textnode.text, None, 
+
+
 main()
