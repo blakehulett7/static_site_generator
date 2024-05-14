7627280602ec972d2f5199b3b0420265d5e0df15
blakehulett7 <blake.hulett7@gmail.com>
2024-05-14 15:25:15 -0500

    img functionality added, still need to unittest. Hope it worksgit add .!

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 def main():
-    textnode = TextNode("This is a text node", "italic")
+    textnode = TextNode("This is a text node", "link", "boot.dev")
     print(textnode_to_htmlnode(textnode))
 
 
@@ -14,9 +14,20 @@ def textnode_to_htmlnode(textnode):
     if textnode.text_type not in valid_types:
         raise Exception("Invalid Type")
     valid_tags = [None, "b", "i", "code", "a", "img"]
-    valid_props = [None, None, None, None, {"href": textnode.url}, {}]
+    valid_props = [
+        None,
+        None,
+        None,
+        None,
+        {"href": textnode.url},
+        {"src": textnode.url, "alt": textnode.text},
+    ]
     type_index = valid_types.index(textnode.text_type)
-    return LeafNode(valid_tags[type_index], textnode.text, None, 
+    if textnode.text_type == "image":
+        textnode.text = ""
+    return LeafNode(
+        valid_tags[type_index], textnode.text, None, valid_props[type_index]
+    )
 
 
 main()
