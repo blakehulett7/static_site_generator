0e74889d8d128126da9abe5962bc9b2b8d66e5a2
blakehulett7 <blake.hulett7@gmail.com>
2024-05-16 10:20:21 -0500

    Extract markdown images and links functions created using regex and behaving as expected. Another one down!

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@ print("Hello World!")
 
 from textnode import TextNode, textnode_to_htmlnode
 from htmlnode import HTMLNode, LeafNode, ParentNode
+import re
 
 
 def main():
-    textnode = TextNode("This is a text node", "link", "boot.dev")
-    print(textnode_to_htmlnode(textnode))
+    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+
+    
+    
 
 
 main()
