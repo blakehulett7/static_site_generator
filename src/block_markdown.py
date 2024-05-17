9a9f509daf4810f5d971ba61b0a4e96db66f084b
blakehulett7 <blake.hulett7@gmail.com>
2024-05-17 11:26:45 -0500

    md to blocks function added, still need to test :)

---
+++ src/block_markdown.py
@@ -0,0 +1,16 @@
+def markdown_to_blocks(md_string):
+    block_list = md_string.split("\n\n")
+    new_list = []
+    for block in block_list:
+        if block != "":
+            new_list.append(block.strip())
+    return new_list
+
+md_string = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is a list item
+* This is another list item"""
+
+print(markdown_to_blocks(md_string))
