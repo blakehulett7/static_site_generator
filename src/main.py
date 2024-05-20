5d5a6dc6565dedfe8f06d4730c16b1fabfb46b58
blakehulett7 <blake.hulett7@gmail.com>
2024-05-19 22:31:08 -0500

    main.py copy directory function started!

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import shutil
 
 
 def main():
-    static_to_public_copy()
+    print(
+        directory_copy(
+            "/home/smirkin_blake/workspace/github.com/blakehulett7/staticsitegenerator/static"
+        )
+    )
 
 
 def static_to_public_copy():
@@ -15,6 +19,23 @@ def static_to_public_copy():
     if os.path.exists(public_directory):
         shutil.rmtree(public_directory)
     os.mkdir(public_directory)
+    directory_copy(static_directory)
+
+
+def directory_copy(directory_path):
+    project_directory = (
+        "/home/smirkin_blake/workspace/github.com/blakehulett7/staticsitegenerator"
+    )
+    target_directory = f"{project_directory}/public"
+    directory_contents = os.listdir(directory_path)
+    copied_contents = []
+    for contents in directory_contents:
+        path = f"{directory_path}/{contents}"
+        if os.path.isfile(f"{directory_path}/{contents}"):
+            shutil.copy(path, public_directory)
+        else:
+            copied_contents.append(directory_copy(path))
+    return copied_contents
 
 
 main()
