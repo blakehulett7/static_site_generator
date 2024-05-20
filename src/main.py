467676d6d1fc771296415557da989973a26bf216
blakehulett7 <blake.hulett7@gmail.com>
2024-05-19 21:57:54 -0500

    started the static to public copy function, testing the new gitignore.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
-print("Hello World!")
-
-from textnode import TextNode, textnode_to_htmlnode
-from htmlnode import HTMLNode, LeafNode, ParentNode
-import re
+import os
+import shutil
 
 
 def main():
-    text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+    static_to_public_copy()
 
-    
-    
+
+def static_to_public_copy():
+    project_directory = (
+        "/home/smirkin_blake/workspace/github.com/blakehulett7/staticsitegenerator"
+    )
+    static_directory = f"{project_directory}/static"
+    public_directory = f"{project_directory}/public"
+    if os.path.exists(public_directory):
+        shutil.rmtree(public_directory)
+    os.mkdir(public_directory)
 
 
 main()
